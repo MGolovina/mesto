3e8f56561144fa2f712672e8825be4ef9942c27f
Maria Golovina <maria.a.golovina@gmail.com>
2021-10-08 10:50:57 -0600

    fix js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,8 @@
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 const editButton = document.querySelector('.profile__edit-button');
-
-
 const formButton = document.querySelector('.popup__button')
-
 const form = document.querySelector('.popup__form');
-
 const nameField = document.querySelector('.popup__input_name');
 const profileName = document.querySelector('.profile__name');
 const aboutField = document.querySelector('.popup__input_about');
@@ -15,27 +11,25 @@ const profileAbout = document.querySelector('.profile__about');
 
 function openPopup() {
     popup.classList.add('popup_opened')
+    nameField.value = profileName.textContent;
+    aboutField.value = profileAbout.textContent;
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
-editButton.addEventListener('click', openPopup)
-
-
-popupCloseButton.addEventListener('click', closePopup)
-//    popup.classList.remove('popup_opened')
 
 function submitForm (event) {
    event.preventDefault()
-
    profileName.textContent = nameField.value;
    profileAbout.textContent = aboutField.value;
-
    closePopup();
 }
 
-
+editButton.addEventListener('click', openPopup)
+popupCloseButton.addEventListener('click', closePopup)
 form.addEventListener('submit', submitForm) 
+
+
 
